a51bd7c3c74469d066646c88e60a600158984450
George Waller <george.waller3@gmail.com>
2020-02-08 18:08:14 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace object_oriented_queue
+{
+    //Program
+    internal class Program
+    {
+        //abstract data type class
+        private class ADT
+        {
+            //decalrations
+            public int memorysize;
+            public int front;
+            public int rear;
+            public string[] memory;
+            public int MemoryStart = 0;
+            
+            //constructor
+            public ADT(int newmemorysize)
+            {
+                memorysize = newmemorysize;
+            }
+            
+            //create memory
+            public Array CreateMemory(int size)
+            {
+                memory = new string[size];
+                front = 0;
+                rear = 0;
+                return memory;
+            }
+
+            //isEmpty
+            public bool isEmpty()
+            {
+                int sum = 0;
+                
+                //check for empty memory
+                for (var i = 0; i < memorysize; i++)
+                {
+                    if (memory[i] == null)
+                    {
+                        sum += 1;
+                        
+                    }
+                }
+
+                if (sum == memorysize)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            //isFull
+            public bool isFull()
+            {
+                int sum = 0;
+
+                //check for full memory
+                for (var i = 0; i < memorysize; i++)
+                {
+                    if (memory[i] != null)
+                    {
+                        sum += 1;
+                    }
+                }
+
+                if (sum == memorysize)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            //return memory
+            public void show()
+            {
+                for (int i = 0; i < memorysize; i++)
+                {
+                    Console.WriteLine("index: " + i + " = " + memory[i]);
+                }
+            }
+        }
+        
+        //inherited queue class
+        private class LinearQueue : ADT
+        {
+            private bool isCirular = false;
+            private bool isPriority = false;
+            
+            //constructor
+            public LinearQueue(int newmemorysize) : base(newmemorysize)
+            {
+                CreateMemory(memorysize);
+            }
+            
+            //enQueue
+            public virtual void enQueue(string newItem)
+            {
+                //if start
+                if (rear == front)
+                {
+                    memory[rear] = newItem;
+                    rear += 1;
+                }
+                //queue full
+                else if (rear == memorysize)
+                {
+                    Console.WriteLine("\n Reached end of queue, do you with to rewrite? (Y/N)\n");
+                    if (Console.ReadLine() == "Y")
+                    {
+                        memory[rear] = newItem;
+                    }
+                }
+                else
+                {
+                    memory[rear] = newItem;
+                    rear += 1;
+                }
+            }
+            
+            //deQueue
+            public virtual void deQueue()
+            {
+                //check empty
+                if (isEmpty())
+                {
+                    Console.WriteLine("\n queue is empty, there are no elements to return\n");
+                    return;
+                }
+                
+                //hold item
+                string item = memory[front];
+                
+                //remove front item
+                memory[front] = null;
+                
+                //shift pointer
+                front += 1;
+            }
+            
+            //isCircular
+            public bool is_Circular()
+            {
+                return isCirular;
+            }
+            
+            //isPriority
+            public bool is_Priority()
+            {
+                return isPriority;
+            }
+            
+            //setCircular
+            public void setCircular(bool set)
+            {
+                isCirular = set;
+            }
+            
+            //setPriority
+            public void setPriority(bool set)
+            {
+                isPriority = set;
+            }
+
+        }
+        
+        //polymorphism circular queue
+        private class CircularQueue : LinearQueue
+        {
+            //constructor
+            public CircularQueue(int newmemorysize) : base(newmemorysize)
+            {
+                CreateMemory(memorysize);
+                setCircular(true);
+            }
+            
+            //enQueue
+            public override void enQueue(string newItem)
+            {
+                //reach end of queue
+                if (rear == memorysize)
+                {
+                    if (memory[MemoryStart] != null)
+                    {
+                        Console.WriteLine("\n There is no empty space, do you with to rewrite? (Y/N)\n");
+                        if (Console.ReadLine() == "Y")
+                        {
+                            rear = MemoryStart;
+                            memory[rear] = newItem;
+                            rear += 1;
+                        } 
+                    }
+                    else
+                    {
+                        rear = MemoryStart;
+                        memory[rear] = newItem;
+                        rear += 1;
+                    }
+                    
+                }
+                else
+                {
+                    if (memory[rear] != null)
+                    {
+                        Console.WriteLine("\n There is no empty space, do you with to rewrite? (Y/N)\n");
+                        if (Console.ReadLine() == "Y")
+                        {
+                            memory[rear] = newItem;
+                            rear += 1; 
+                        }
+                    }
+                    else
+                    {
+                        memory[rear] = newItem;
+                        rear += 1;
+                    }
+                }
+            }
+            
+            //deQueue
+            public override void deQueue()
+            {
+                //check empty
+                if (isEmpty())
+                {
+                    Console.WriteLine("\n queue is empty, there are no elements to return\n");
+                    return;
+                }
+                
+                //hold item
+                string item = memory[front];
+                
+                //remove front item
+                memory[front] = null;
+                
+                //shift pointer
+                if (front == memorysize)
+                {
+                    front = MemoryStart;
+                }
+                else
+                {
+                    front += 1; 
+                }
+                Console.WriteLine(front + " dequeue front");
+            }
+        }
+
+        //entry point
+        public static void Main(string[] args)
+        {
+            //linear Queue
+            Console.WriteLine("Linear queue: ");
+            var q = new LinearQueue(6);
+            Console.WriteLine("is empty = " + q.isEmpty().ToString());
+            q.enQueue("orange");
+            q.enQueue("red");
+            q.enQueue("blue");
+            q.enQueue("orange");
+            q.enQueue("red");
+            q.enQueue("blue");
+            q.show();
+            Console.WriteLine(q.isFull().ToString());
+            q.deQueue();
+            q.show();
+            Console.WriteLine(q.isFull().ToString());
+
+            //circular queue
+            Console.WriteLine("\n\nCircular Queue");
+            var q2 = new CircularQueue(4);
+            Console.WriteLine("is empty = " + q2.isEmpty().ToString());
+            q2.enQueue("orange");
+            q2.enQueue("red");
+            q2.show();
+            q2.deQueue();
+            q2.show();
+            q2.enQueue("red");
+            q2.enQueue("blue");
+            q2.enQueue("orange");
+            q2.enQueue("orange");
+            q2.show();
+            
+            
+            
+        }
+    }
+}
